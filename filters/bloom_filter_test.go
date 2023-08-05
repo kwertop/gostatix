a7a4c21b632f40d95a0c30b0ba360f37d063792e
Rahul Sharma <rahulveera2009@gmail.com>
2023-08-05 21:38:49 +0530

    added some more test cases for bloom filter

--- filters/bloom_filter_test.go
+++ filters/bloom_filter_test.go
@@ -1,6 +1,7 @@
 package filters
 
 import (
+	"encoding/binary"
 	"testing"
 
 	"github.com/gostatix/bitset"
@@ -38,5 +39,135 @@ func TestFilterWithBitSetMem(t *testing.T) {
 	}
 	if !ok4 {
 		t.Errorf("%v should be in filter", b3)
+	}
+}
+
+func TestFilterZeroSizes(t *testing.T) {
+	bitset := bitset.NewBitSetMem(0)
+	filter, _ := NewBloomFilterWithBitSet(0, 0, bitset)
+	if filter.GetCap() != 1 {
+		t.Errorf("size: %v should be 1", filter.GetCap())
+	}
+	if filter.GetNumHashes() != 1 {
+		t.Errorf("numHash: %v should be 1", filter.GetNumHashes())
+	}
+}
+
+func TestInt32(t *testing.T) {
+	bitset := bitset.NewBitSetMem(1000)
+	filter, _ := NewBloomFilterWithBitSet(1000, 4, bitset)
+	e1 := make([]byte, 4)
+	e2 := make([]byte, 4)
+	e3 := make([]byte, 4)
+	binary.BigEndian.PutUint32(e1, 100)
+	binary.BigEndian.PutUint32(e2, 101)
+	binary.BigEndian.PutUint32(e3, 102)
+	filter.Insert(e1)
+	ok1 := filter.Lookup(e1)
+	ok2 := filter.Lookup(e2)
+	filter.Insert(e3)
+	ok3 := filter.Lookup(e3)
+	if !ok1 {
+		t.Errorf("%v should be in filter", e1)
+	}
+	if ok2 {
+		t.Errorf("%v should not be in filter", e2)
+	}
+	if !ok3 {
+		t.Errorf("%v should be in filter", e3)
+	}
+}
+
+func TestStringInFilter(t *testing.T) {
+	bitset := bitset.NewBitSetMem(1000)
+	filter, _ := NewBloomFilterWithBitSet(1000, 4, bitset)
+	e1 := "This"
+	e2 := "is"
+	e3 := "present"
+	e4 := "in"
+	e5 := "bloom"
+	filter.InsertString(e1)
+	ok1 := filter.LookupString(e1)
+	ok2 := filter.LookupString(e2)
+	filter.InsertString(e3)
+	ok3 := filter.LookupString(e3)
+	ok4 := filter.LookupString(e4)
+	filter.InsertString(e5)
+	if !ok1 {
+		t.Errorf("%v should be in filter", e1)
+	}
+	if ok2 {
+		t.Errorf("%v should not be in filter", e2)
+	}
+	if !ok3 {
+		t.Errorf("%v should be in filter", e3)
+	}
+	if ok4 {
+		t.Errorf("%v should not be in filter", e4)
+	}
+}
+
+func testPositiveRate(nItems uint, errorRate float64, t *testing.T) {
+	filter, _ := NewMemBloomFilterWithParameters(nItems, errorRate)
+	e := make([]byte, 4)
+	for i := uint32(0); i < uint32(nItems); i++ {
+		binary.BigEndian.PutUint32(e, i)
+		filter.Insert(e)
+	}
+	estimatedErrorRate := filter.BloomPositiveRate()
+	if estimatedErrorRate > 1.1*errorRate {
+		t.Errorf("estimated error rate %v too high for nItems %v and expected error rate %v", estimatedErrorRate, nItems, errorRate)
+	}
+}
+
+func TestPositiveRate1000_0001(t *testing.T) {
+	testPositiveRate(1000, 0.001, t)
+}
+
+func TestPositiveRate10000_0001(t *testing.T) {
+	testPositiveRate(10000, 0.001, t)
+}
+
+func TestPositiveRate100000_0001(t *testing.T) {
+	testPositiveRate(100000, 0.001, t)
+}
+
+func TestPositiveRate1000_001(t *testing.T) {
+	testPositiveRate(1000, 0.01, t)
+}
+
+func TestPositiveRate10000_001(t *testing.T) {
+	testPositiveRate(10000, 0.01, t)
+}
+
+func TestPositiveRate100000_001(t *testing.T) {
+	testPositiveRate(100000, 0.01, t)
+}
+
+func TestPositiveRate1000_01(t *testing.T) {
+	testPositiveRate(1000, 0.1, t)
+}
+
+func TestPositiveRate10000_01(t *testing.T) {
+	testPositiveRate(10000, 0.1, t)
+}
+
+func TestPositiveRate100000_01(t *testing.T) {
+	testPositiveRate(100000, 0.1, t)
+}
+
+func TestGetSize(t *testing.T) {
+	bitset := bitset.NewBitSetMem(1000)
+	filter, _ := NewBloomFilterWithBitSet(1000, 4, bitset)
+	if filter.GetCap() != filter.size {
+		t.Errorf("getcap method return value %v doesn't match with filter size %v", filter.GetCap(), filter.size)
+	}
+}
+
+func TestGetNumHashes(t *testing.T) {
+	bitset := bitset.NewBitSetMem(1000)
+	filter, _ := NewBloomFilterWithBitSet(1000, 4, bitset)
+	if filter.GetNumHashes() != filter.numHashes {
+		t.Errorf("getnumhashes method return value %v doesn't match with filter numHashes %v", filter.GetNumHashes(), filter.numHashes)
 	}
 }
